661285202485ff6ccc2e5b63d2820ae349d54540
Henning Emil Bøjer Bach <henning@thehenning.xyz>
2026-06-28 14:30:42 +0200

    Overhaul Fibonacci example

--- examples/fib.asm
+++ examples/fib.asm
@@ -1,13 +1,18 @@
 ; Basic fibonacci
 start:
-  addi r1, r0, 0    ; a = 0
-  addi r2, r0, 1    ; b = 1
-  addi r4, r0, 5    ; d = 5, n iterations
-loop:
-  add  r3, r1, r2   ; c = a + b
-  mv   r1, r2       ; a = b
-  mv   r2, r3       ; b = c
+  addi r1, r0, 0    ; r1 = F_0 = 0
+  addi r2, r0, 1    ; r2 = F_1 = 1
+  addi r4, r0, 5    ; r4 = n: 5 iterations
+; iteration k = 1
+iterate:
+  add  r3, r1, r2   ; r3 = r1 + r2 = F_{k+1}
+  ; bail early if last iteration
+  addi r4, r4, 0xff ; r4 -= 1
+  bz   end          ; if r4 == 0, jump out
+  ; setup for iteration k+1
+  mv   r1, r2       ; r1 = F_k
+  mv   r2, r3       ; r2 = F_{k+1}
+  jmp  iterate      ; jump for iteration k+1
 
-  addi r4, r4, 0xff ; d -= 1
-  bnz  loop         ; jump to loop if not done
-  halt              ; done, result in r3
+end:
+  halt              ; done, r3 = F_{n+1}
